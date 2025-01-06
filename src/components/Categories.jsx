70eaa2777e7f833014f819a7ff520cd9ae31d517
Your Name <golamsarwar1896@gmail.com>
2025-01-06 07:04:30 +0600

    fixed the category. Will work on the card size no

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,17 +3,20 @@ import { NavLink } from "react-router-dom";
 const Categories = ({ categories }) => {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-center mt-16 md:w-[50%] mx-auto">
-        Visit Different Sport Equipment{" "}
-        <span className="text-amber-500 text-center text-5xl">Categories</span>
+      <h1 className="md:text-4xl text-xl font-bold text-center mt-16 md:w-[80%] mx-auto">
+        Visit Different Sport Equipment
+        <br />
+        <span className="text-amber-500 text-center md:text-5xl text-4xl">
+          Categories
+        </span>
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center text-center p-10 lg:gap-32 gap-10 md:my-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center text-center md:p-12 p-16  lg:gap-20 gap-10 md:my-6">
         {categories.map((category) => (
           <NavLink
             className={({ isActive }) =>
               isActive
                 ? "bg-amber-500 border-none  text-cyan-950  md:px-10 md:py-4 px-3 py-1 rounded-full"
-                : "bg-cyan-950 border-none  text-white  md:px-10 md:py-4 px-3 py-1 rounded-full"
+                : "bg-cyan-950 border-none  text-amber-500  md:px-10 md:py-4 px-3 py-1 rounded-full"
             }
             key={category.category}
             to={`/category/${category.category}`}
